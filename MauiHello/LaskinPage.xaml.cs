407bd2a3674a9c884ca4fa663aa4b971b1f03842
Pont-SimoSiren <sirensimo7@gmail.com>
2024-11-26 12:30:02 +0200

    ok

--- MauiHello/LaskinPage.xaml.cs
+++ MauiHello/LaskinPage.xaml.cs
@@ -1,3 +1,6 @@
+using System.Data;
+using System.Runtime.Intrinsics.X86;
+
 namespace MauiHello;
 
 public partial class LaskinPage : ContentPage
@@ -5,10 +8,52 @@ public partial class LaskinPage : ContentPage
 	public LaskinPage()
 	{
 		InitializeComponent();
+        display_field.Text = "";
 	}
 
-    private void Button_Clicked(object sender, EventArgs e)
-    {
 
+    private void LaskeTulos()
+    {
+        try
+        {
+            string kaava = display_field.Text;
+            var table = new DataTable();
+            var result = table.Compute(kaava, string.Empty);
+            display_field.Text = result.ToString();
+        }
+        catch {
+
+           DisplayAlert("Virhe", "Tyhjennet‰‰n kaava", "ok");
+           display_field.Text = "";
+
+        }
     }
+
+
+    private void Button_Clicked(object sender, EventArgs e)
+	{
+        //  object sender on geneerinen ilmaisu ja koska tied‰mme kyseess‰ olevan buttonin voimme tehd‰
+		// objektin tietotyypin muunnoksen eli cast:in buttoniksi.
+
+        var button = sender as Button;
+        //var button = (Button)sender; <--  vaihtoehtoinen syntaksi castin tekemiseen
+
+        string btnText = button.Text;
+
+        switch (btnText)
+        {
+            case "=":
+                LaskeTulos();
+                break;
+
+            case "C":
+                display_field.Text = "";
+                break;
+
+            default:
+                display_field.Text += btnText;
+                break;
+        }
+    }
+
 }
